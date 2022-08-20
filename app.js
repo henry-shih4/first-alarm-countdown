078cc90249b8e1cbf525053dda02f10b9a9a19d3
henry-shih4 <100496651+henry-shih4@users.noreply.github.com>
2022-08-19 19:31:24 -0500

    update timer
    added seconds input feature,
    added background img from unsplash,
    added styling to elements.

--- app.js
+++ app.js
@@ -1,96 +1,111 @@
-let hourInput = document.getElementById('hour');
-let minInput = document.getElementById('minute');
-let hour = document.getElementById('hour').value;
-let minute = document.getElementById('minute').value;
-let startBtn = document.getElementById('start-button');
+addEventListener("DOMContentLoaded", resetClock);
+
+let hourInput = document.getElementById("hour");
+let minInput = document.getElementById("minute");
+let secondInput = document.getElementById("seconds");
+let hour = document.getElementById("hour").value;
+let minute = document.getElementById("minute").value;
+let second = document.getElementById("seconds").value;
+let startBtn = document.getElementById("start-button");
 let secondStart = 59;
-let hourDisplay = document.getElementById('hour-display');
-let minuteDisplay = document.getElementById('minute-display');
-let secondDisplay = document.getElementById('second-display');
-let pauseBtn = document.getElementById('pause-button');
-let resetBtn = document.getElementById('reset-button');
-let resumeBtn = document.getElementById('resume-button');
+let hourDisplay = document.getElementById("hour-display");
+let minuteDisplay = document.getElementById("minute-display");
+let secondDisplay = document.getElementById("second-display");
+let pauseBtn = document.getElementById("pause-button");
+let resetBtn = document.getElementById("reset-button");
+let resumeBtn = document.getElementById("resume-button");
 let resumedHour;
 let resumedMin;
 let resumedSec;
 let myInterval;
 
-startBtn.addEventListener('click', startTime);
+startBtn.addEventListener("click", startTime);
 
-
-function startTime(){
-    myStop();
-    let hour = document.getElementById('hour').value;
-    let minute = document.getElementById('minute').value;
-    if (hour == 0 && minute ==0){
-        alert('please enter a time')
-        return;
-    }
+function startTime() {
+  myStop();
+  let hour = document.getElementById("hour").value;
+  let minute = document.getElementById("minute").value;
+  let second = document.getElementById("seconds").value;
+  if (hour == 0 && minute == 0 && second == 0) {
+    alert("please enter a time");
+    return;
+  }
+  if (minute == 0) {
+    minuteDisplay.innerHTML = 59;
+    hourDisplay.innerHTML = hour - 1;
+  } else {
+    minuteDisplay.innerHTML = minute - 1;
     hourDisplay.innerHTML = hour;
-    minuteDisplay.innerHTML = minute-1;
-    secondDisplay.innerHTML = secondStart;
+  }
 
-myInterval = setInterval(runClock, 1000);
-  pauseBtn.addEventListener('click', myStop);
-  resumeBtn.addEventListener('click', myResume);
-  resetBtn.addEventListener('click', resetClock);
+  if (second > 0) {
+    minuteDisplay.innerHTML = minute;
+    hourDisplay.innerHTML = hour;
+    secondDisplay.innerHTML = second;
+  } else {
+    secondDisplay.innerHTML = secondStart;
+  }
+
+  myInterval = setInterval(runClock, 1000);
+  pauseBtn.addEventListener("click", myStop);
+  resumeBtn.addEventListener("click", myResume);
+  resetBtn.addEventListener("click", resetClock);
 }
 
-
-
-function runClock(){
-    secondDisplay.innerHTML--;
-    if (secondDisplay.innerHTML<0){
-      secondDisplay.innerHTML = secondStart
-      minuteDisplay.innerHTML--;
-    }
-    if (minuteDisplay.innerHTML<0){
-        minuteDisplay.innerHTML = secondStart;
-        hourDisplay.innerHTML--;
-      }
-      if (hourDisplay.innerHTML<0){
-        hourDisplay.innerHTML = 0;
-      }
-      if((hourDisplay.innerHTML==0) && (minuteDisplay.innerHTML==0) &&(secondDisplay.innerHTML==0)){
-        alert('Timer done');
-        resetClock();
-    }
-
+function runClock() {
+  secondDisplay.innerHTML--;
+  if (secondDisplay.innerHTML < 0) {
+    secondDisplay.innerHTML = secondStart;
+    minuteDisplay.innerHTML--;
   }
-
+  if (minuteDisplay.innerHTML < 0) {
+    minuteDisplay.innerHTML = secondStart;
+    hourDisplay.innerHTML--;
+  }
+  if (hourDisplay.innerHTML < 0) {
+    hourDisplay.innerHTML = 0;
+  }
+  if (
+    hourDisplay.innerHTML == 0 &&
+    minuteDisplay.innerHTML == 0 &&
+    secondDisplay.innerHTML == 0
+  ) {
+    alert("Timer done");
+    resetClock();
+  }
+}
 
 function myResume() {
-    myStop();
-    secondDisplay.innerHTML = resumedSec; 
-    minuteDisplay.innerHTML = resumedMin;
-    hourDisplay.innerHTML = resumedHour;
-    myInterval = setInterval(runClock, 1000);
-  }
-
+  myStop();
+  secondDisplay.innerHTML = resumedSec;
+  minuteDisplay.innerHTML = resumedMin;
+  hourDisplay.innerHTML = resumedHour;
+  myInterval = setInterval(runClock, 1000);
+}
 
 function myStop() {
-    clearInterval(myInterval);    
-resumedHour = hourDisplay.innerHTML;
-resumedMin = minuteDisplay.innerHTML
-resumedSec = secondDisplay.innerHTML;
+  clearInterval(myInterval);
+  resumedHour = hourDisplay.innerHTML;
+  resumedMin = minuteDisplay.innerHTML;
+  resumedSec = secondDisplay.innerHTML;
+}
+
+function resetClock() {
+  hourInput.value = 0;
+  minInput.value = 0;
+  secondInput.value = 0;
+  hourDisplay.innerHTML = "00";
+  minuteDisplay.innerHTML = "00";
+  secondDisplay.innerHTML = "00";
+  myStop();
+}
+
+function timerDone() {
+  if (
+    (hourDisplay.innerHTML = 0) &&
+    (minuteDisplay.innerHTML = 0) &&
+    (secondDisplay.innerHTML = 0)
+  ) {
+    alert("Time's up!");
   }
-
-
-function resetClock(){
-    hourInput.value = 0;
-    minInput.value = 0;
-    hourDisplay.innerHTML = '00';
-    minuteDisplay.innerHTML ='00';
-    secondDisplay.innerHTML ='00';
-    myStop();
-  }
-
-
-  function timerDone(){
-    if((hourDisplay.innerHTML=0) && (minuteDisplay.innerHTML=0) &&(secondDisplay.innerHTML=0)){
-        alert('Timer done!');
-    }
-  }
-
-
-
+}
